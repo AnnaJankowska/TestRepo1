f949a8a1a137233dba4742f2d34e1b7b9cb794a7
AnnaJankowska <57603365+AnnaJankowska@users.noreply.github.com>
2019-11-11 00:38:25 +0100

    cos tam

---
+++ HelloWorld/HelloWorld/Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Class2
+    {
+        public void ZmienImie(string mojeImie)
+        {
+            mojeImie = "hahah zmienione";
+        }
+
+
+        public void ZmienImie(Class1 mojaKlasa)
+        {
+            mojaKlasa.Imie = "hahah zmienione";
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            string imie = "Dawid";
+            var mojaKls = new Class1();
+            mojaKls.Imie = "Dawid";
+
+            var klasa = new Class2();
+            klasa.ZmienImie(mojaKls);
+            klasa.ZmienImie(imie);
+            Console.WriteLine(mojaKls.Imie);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
